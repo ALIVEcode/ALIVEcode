be256dc34bba65a3b357393512d3e4a60aa86d7e
Enric <mosk3002@gmail.com>
2022-02-16 16:25:06 -0500

    ADD - Start of activity model revamp

--- backend/src/models/course/entities/activity.entity.ts
+++ backend/src/models/course/entities/activity.entity.ts
@@ -1,18 +1,29 @@
 import { Exclude, Type } from 'class-transformer';
 import { IsEmpty, IsNotEmpty, Length } from 'class-validator';
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, TableInheritance } from 'typeorm';
 import { ActivityLevelEntity } from './activity_level.entity';
 
 export class ActivityContent {
   body: string;
 }
 
+export enum ACTIVIY_TYPE {
+  VIDEO,
+  THEORY,
+  LEVEL,
+}
+
 @Entity()
+@TableInheritance({ column: { type: 'enum', name: 'type', enum: ACTIVIY_TYPE } })
 export class ActivityEntity {
   @PrimaryGeneratedColumn('increment')
   @Exclude({ toClassOnly: true })
   @IsEmpty()
   id: number;
+
+  @Exclude({ toClassOnly: true })
+  @IsEmpty()
+  type: ACTIVIY_TYPE;
 
   @IsNotEmpty()
   @Column({ nullable: false })

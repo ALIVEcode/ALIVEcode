32a0fefa054d7bf87f023830f4b1159a7150a730
Félix Jobin <fjobin007@gmail.com>
2022-02-28 22:59:43 -0500

    ADDED DOCUMENTATION FOR FUNCTIONS AND NEURON
    Documentation was added for both files and the ActivationFunction property was removed from the Neuron class.

--- frontend/src/Pages/Level/LevelAI/artificial_intelligence/ai_functions/Function.ts
+++ frontend/src/Pages/Level/LevelAI/artificial_intelligence/ai_functions/Function.ts
@@ -1,42 +1,89 @@
-import { create, all } from 'mathjs';
-const config = {};
-const math = create(all, config);
+import { Matrix } from "../AIUtils";
 
+/**
+ * This class contains sll the activation functions available in the AI module.
+ * The functions created with that class can be used to compute the activation
+ * function on each element of a Matrix object.
+ * 
+ * The activation functions implemented are :
+ * - the Rectified Linear Unit function;
+ * - the Sigmoid funciton;
+ * - the Tangent Hyperbolic function.
+ */
 export class ActivationFunction {
 
   static RELU: number = 0;
   static SIGMOID: number = 1;
   static TANH: number = 2;
 
+  /**
+   * Creates an activation function of the type specified.
+   * 
+   * @param type the type of activation function. It can be :
+   * - ActivationFunction.RELU for a Rectified Linear Unit function;
+   * - ActivationFunction.SIGMOID for a Sigmoid function;
+   * - ActivationFunction.TANH for a Tangent Hyperbolic function.
+   */
   constructor(
     private type: number
   ) {}
 
-  public compute(input: number): number {
-    let output: number = 0;
+  /**
+   * Computes the activation function on each element of a Matrix object.
+   * 
+   * @param inputs the input Matrix.
+   * @returns a new Matrix which is computed by applying the activation function to 
+   * each element of the input Matrix.
+   */
+  public compute(inputs: Matrix): Matrix {
+    const inputValue: number[][] = inputs.getValue();
     
-    switch (this.type) {
-      case ActivationFunction.RELU:
-        output = this.computeReLU(input);
-        break;
-      case ActivationFunction.SIGMOID:
-        output = this.computeSigmoid(input);
-        break;
-      case ActivationFunction.TANH:
-        output = this.computeTanH(input);
-    }
-
-    return output;
+    const outputValue: number[][] = inputValue.map(
+      (value: number[], index: number): number[] => {
+        let mapOutput: number[] = [0];
+        switch (this.type) {
+          case ActivationFunction.RELU:
+            mapOutput = value.map(this.computeReLU);
+            break;
+          case ActivationFunction.SIGMOID:
+            mapOutput = value.map(this.computeSigmoid);
+            break;
+          case ActivationFunction.TANH:
+            mapOutput = value.map(this.computeTanH);
+        }
+        return mapOutput;
+      }
+    );
+    
+    return new Matrix(outputValue);
   }
 
+  /**
+   * Computes the Rectified Linear Unit function on a single input.
+   * 
+   * @param input the input number of the function.
+   * @returns the corresponding output.
+   */
   private computeReLU(input: number): number {
     return Math.max(0, input);
   }
 
+  /**
+   * Computes the Sigmoid function on a single input.
+   * 
+   * @param input the input number of the function.
+   * @returns the corresponding output.
+   */
   private computeSigmoid(input: number): number {
     return 1 / (1 + Math.exp(-input));
   }
 
+  /**
+   * Computes the Tangent Hyperbolic function on a single input.
+   * 
+   * @param input the input number of the function.
+   * @returns the corresponding output.
+   */
   private computeTanH(input: number): number {
     return (Math.exp(input) - Math.exp(-input)) / (Math.exp(input) + Math.exp(-input));
   }
@@ -47,10 +94,6 @@ export class CostFunction {
 
   static MSE: number = 0;
   static MAE: number = 0;
-
-  public compute(inputs: number[]): number {
-    
-  }
 
   
 }
--- frontend/src/Pages/Level/LevelAI/artificial_intelligence/ai_models/ai_neural_networks/Neuron.ts
+++ frontend/src/Pages/Level/LevelAI/artificial_intelligence/ai_models/ai_neural_networks/Neuron.ts
@@ -1,25 +1,35 @@
-import { Matrix, zeros, matMul } from "../../AIUtils";
-import { ActivationFunction } from "../../ai_functions/Function";
+import { Matrix, matMul } from "../../AIUtils";
 
-// Configuration for the matrix library
-
-
+/**
+ * This class represents the core of a neuron in a neural network. 
+ * It contains its associated weights and bias, and a method that can compute 
+ * an output Matrix based on an input Matrix.
+ */
 export default class Neuron {
 
+  /**
+   * Creates a Neuron by specifying its weight and bias values.
+   * @param bias the bias value.
+   * @param weights the weights values, as a 1-column Matrix object.
+   */
   constructor(
     private bias: number,
-    private weights: Matrix,
-    private activation: ActivationFunction
-  ) {}
+    private weights: Matrix
+  ) {
+    if (weights.getRows() > 1) {
+      weights.setValue([weights.getValue()[0]]);
+    }
+  }
 
   /**
    * Computes the output of the neuron based on the given inputs and 
-   * its parameters. Also applies the layer
+   * its parameters. TODO Also applies the Activation function of its layer.
    * @param inputs the inputs of the neuron.
    * @returns the computed output
    */
   public computeOutput(inputs: Matrix): Matrix {
-    return new Matrix(1, 1);
+    let output: Matrix = matMul(inputs, this.weights);
+    return output;
   }
 
 }

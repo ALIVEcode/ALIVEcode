5f59507e590b4e82c2f3ef5176c43c38d74c3960
Enric <mosk3002@gmail.com>
2022-03-04 15:05:59 -0500

    ADD - Start of CourseBody with activity content

--- frontend/src/Components/CourseComponents/CourseBody/CourseBody.tsx
+++ frontend/src/Components/CourseComponents/CourseBody/CourseBody.tsx
@@ -1,16 +1,24 @@
 import { useContext } from 'react';
 import { CourseContext } from '../../../state/contexts/CourseContext';
+
 const CourseBody = () => {
 	const { openedActivity: activity } = useContext(CourseContext);
 
 	return (
-		<div className="w-full h-full flex justify-center items-center">
+		<>
 			{!activity ? (
-				<label>This will be the loaded activity</label>
+				<div className="w-full h-full flex justify-center items-center">
+					<label>This will be the loaded activity</label>
+				</div>
 			) : (
-				<div>{activity.name}</div>
+				<div className="w-full h-full flex flex-col overflow-y-auto">
+					<div className="sticky text-2xl text-center py-8 w-full">
+						{activity.name}
+					</div>
+					<div className="bg-blue-100 h-[1500px]"></div>
+				</div>
 			)}
-		</div>
+		</>
 	);
 };
 
